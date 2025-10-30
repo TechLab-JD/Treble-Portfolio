5e01b6e441cf5fd51e7d28ab6d45903a5210620d
J.D. <techlab.jd@gmail.com>
2025-10-30 13:50:07 -0500

    clean up structure

--- js/themes.js
+++ js/themes.js
@@ -3,7 +3,10 @@ const themes = [
     font: "'Poppins', sans-serif",
     colors: { background: "#0a0a0a", text: "#fff" },
     content: {
-      header: "<h1>Samual B Hodgkinson</h1><h3>Illustrations</h3>",
+      header: `
+      <h1>Samual B Hodgkinson</h1>
+      <h3>Illustrations</h3>
+      `,
       main: "Image Panel 1",
       side: "Details about Panel 1"
     }
@@ -12,7 +15,10 @@ const themes = [
     font: "'Merriweather', serif",
     colors: { background: "#838383ff", text: "#111" },
     content: {
-      header: "<h1>Samual B Hodgkinson</h1><h3>Design</h3>",
+      header: `
+      <h1>Samual B Hodgkinson</h1>
+      <h3>Illustrations</h3>
+      `,
       main: "Image Panel 2",
       side: "Details about Panel 2"
     }
@@ -21,7 +27,10 @@ const themes = [
     font: "'Merriweather', serif",
     colors: { background: "#585757ff", text: "#111" },
     content: {
-      header: "<h1>Samual B Hodgkinson</h1><h3>UI/UX</h3>",
+      header: `
+      <h1>Samual B Hodgkinson</h1>
+      <h3>Illustrations</h3>
+      `,
       main: "Image Panel 3",
       side: "Details about Panel 3"
     }
