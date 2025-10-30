0eb1706e31a350211454a44c6526fca29e471ec5
J.D. <techlab.jd@gmail.com>
2025-10-30 13:35:59 -0500

    readjust elements and fillers

--- js/themes.js
+++ js/themes.js
@@ -3,16 +3,16 @@ const themes = [
     font: "'Poppins', sans-serif",
     colors: { background: "#0a0a0a", text: "#fff" },
     content: {
-      main: "Welcome to Theme 1",
-      side: "Details about Theme 1"
+      main: "Image Panel 1",
+      side: "Details about Panel 1"
     }
   },
   {
     font: "'Merriweather', serif",
     colors: { background: "#fafafa", text: "#111" },
     content: {
-      main: "Now viewing Theme 2",
-      side: "Elegant and clean"
+      main: "Image Panel 1",
+      side: "Details about Panel 1"
     }
   }
 ];
