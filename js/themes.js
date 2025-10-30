c69826ad6222588389aaa7695de38712e70af0e4
J.D. <techlab.jd@gmail.com>
2025-10-30 14:06:56 -0500

    move buttons

--- js/themes.js
+++ js/themes.js
@@ -1,17 +1,13 @@
 const themes = [
   {
     font: "'Poppins', sans-serif",
-    colors: { background: "#0a0a0a", text: "#fff" },
+    colors: { background: "#0a0a0a", text: "#fff", color: "#fff" },
     content: {
       header: `
       <h1>Samual B Hodgkinson</h1>
       <h3>Illustrations</h3>
       <a href="#">About</a>
-      <a href="#">Contact</a>      
-      <div class="carousel-controls">
-        <button id="prevBtn" class="carousel-btn left">⟨</button>
-        <button id="nextBtn" class="carousel-btn right">⟩</button>
-      </div>               
+      <a href="#">Contact</a>               
       `,
       main: "Image Panel 1",
       side: "Details about Panel 1"
@@ -25,11 +21,7 @@ const themes = [
       <h1>Samual B Hodgkinson</h1>
       <h3>Illustrations</h3>
       <a href="#">About</a>
-      <a href="#">Contact</a>   
-      <div class="carousel-controls">
-        <button id="prevBtn" class="carousel-btn left">⟨</button>
-        <button id="nextBtn" class="carousel-btn right">⟩</button>
-      </div>                
+      <a href="#">Contact</a>                  
       `,
       main: "Image Panel 2",
       side: "Details about Panel 2"
@@ -43,11 +35,7 @@ const themes = [
       <h1>Samual B Hodgkinson</h1>
       <h3>Illustrations</h3>
       <a href="#">About</a>
-      <a href="#">Contact</a>   
-      <div class="carousel-controls">
-        <button id="prevBtn" class="carousel-btn left">⟨</button>
-        <button id="nextBtn" class="carousel-btn right">⟩</button>
-      </div>          
+      <a href="#">Contact</a>       
       `,
       main: "Image Panel 3",
       side: "Details about Panel 3"
